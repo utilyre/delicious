adf0b6672119a6142503ab6fce701e10c9b23af4
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-04-15 22:09:00 +0430

    Implement splide effect for recipe cards in Popular component

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
 
 import styles from './Popular.module.css'
 
@@ -18,14 +20,26 @@ const Popular = () => {
   return (
     <div className={styles.container}>
       <h3>Popular Picks</h3>
-      {recipes?.map((recipe) => {
-        return (
-          <div key={recipe.id} className={styles.recipeCard}>
-            <p>{recipe.title}</p>
-            <img src={recipe.image} alt={recipe.title} />
-          </div>
-        )
-      })}
+
+      <Splide
+        options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: 'free',
+          gap: '5rem',
+        }}
+      >
+        {recipes?.map((recipe) => {
+          return (
+            <SplideSlide key={recipe.id} className={styles.recipeCard}>
+              <p>{recipe.title}</p>
+              <img src={recipe.image} alt={recipe.title} />
+              <div className={styles.recipeCardGradient}></div>
+            </SplideSlide>
+          )
+        })}
+      </Splide>
     </div>
   )
 }
