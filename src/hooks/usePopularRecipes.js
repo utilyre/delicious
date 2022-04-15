2c5f9664100e95ce4aba2d223b02664b91ead9ca
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-04-15 22:46:22 +0430

    Implement caching feature to usePopularRecipes
    It'll save the response of API call into localStorage from now on.
    The cache data will expire within an hour.

--- src/hooks/usePopularRecipes.js
+++ src/hooks/usePopularRecipes.js
@@ -1,12 +1,28 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const LS_POPULAR_RECIPES = 'POPULAR_RECIPES'
+
 const usePopularRecipes = () => {
   const [recipes, setRecipes] = useState(null)
 
   const fetchRecipes = async () => {
-    const response = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_API_KEY}&number=9`)
-    setRecipes(response.data.recipes)
+    const rawCache = localStorage.getItem(LS_POPULAR_RECIPES)
+    if (rawCache) {
+      const parsedCache = JSON.parse(rawCache)
+      if (Date.now() - parsedCache.updatedAt < 3600000) return setRecipes(parsedCache.data)
+    }
+
+    const { data } = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_API_KEY}&number=16`)
+    setRecipes(data.recipes)
+
+    localStorage.setItem(
+      LS_POPULAR_RECIPES,
+      JSON.stringify({
+        updatedAt: Date.now(),
+        data: data.recipes,
+      })
+    )
   }
 
   useEffect(() => {
