c1fae5d8fc23fbf51c0ea0f751d532fad9b53bae
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-04-16 22:30:39 +0430

    Add styling to Category component

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -5,24 +5,26 @@ import { GiNoodles, GiChopsticks } from 'react-icons/gi'
 import styles from './Category.module.css'
 
 const Category = () => {
+  const navLinkClassName = ({ isActive }) => isActive ? `${styles.navLink} ${styles.navLinkActive}` : styles.navLink
+
   return (
     <div className={styles.list}>
-      <NavLink to='/cuisine/Italian'>
+      <NavLink to='/cuisine/Italian' className={navLinkClassName}>
         <FaPizzaSlice />
         <h4>Italian</h4>
       </NavLink>
 
-      <NavLink to='/cuisine/American'>
+      <NavLink to='/cuisine/American' className={navLinkClassName}>
         <FaHamburger />
         <h4>American</h4>
       </NavLink>
 
-      <NavLink to='/cuisine/Thai'>
+      <NavLink to='/cuisine/Thai' className={navLinkClassName}>
         <GiNoodles />
         <h4>Thai</h4>
       </NavLink>
 
-      <NavLink to='/cuisine/Japanese'>
+      <NavLink to='/cuisine/Japanese' className={navLinkClassName}>
         <GiChopsticks />
         <h4>Japanese</h4>
       </NavLink>
