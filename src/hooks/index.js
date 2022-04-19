58589c11e994925cea6c3e24c85fbe1f2a1b104a
Amirabbas Fazelinia <utilyre@protonmail.com>
2022-04-19 19:59:48 +0430

    Create a general useQuery hook for any kind of API call

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,10 +2,12 @@ import useVeggieRecipes from './useVeggieRecipes'
 import usePopularRecipes from './usePopularRecipes'
 import useCuisineRecipes from './useCuisineRecipes'
 import useQueryRecipes from './useQueryRecipes'
+import useQuery from './useQuery'
 
 export {
   useVeggieRecipes,
   usePopularRecipes,
   useCuisineRecipes,
   useQueryRecipes,
+  useQuery,
 }
---
+++ src/hooks/useQuery.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import { digest } from '../utils'
+
+const useQuery = (url, opts = { params: {}, cacheTimeout: 3600000, deps: [] }) => {
+  const [data, setData] = useState(null)
+
+  const fetchData = async () => {
+    const cacheKey = await digest(`${url}:${JSON.stringify(opts.params)}`)
+    const rawCache = localStorage.getItem(cacheKey)
+    if (rawCache) {
+      const parsedCache = JSON.parse(rawCache)
+      if (Date.now() - parsedCache.updatedAt <= opts.cacheTimeout) return setData(parsedCache.data)
+    }
+
+    try {
+      const { data } = await axios.get(url, { params: opts.params })
+
+      localStorage.setItem(cacheKey, JSON.stringify({ updatedAt: Date.now(), data }))
+      setData(data)
+    } catch ({ message }) {
+      console.error(message);
+    }
+  }
+
+  useEffect(() => { fetchData() }, opts.deps)
+  return [data, fetchData]
+}
+
+export default useQuery
